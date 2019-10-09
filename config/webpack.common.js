2fdc333ca804537cad3200904ba81334bd7f01b4
youLookLikeDelicious <1059366817@qq.com>
2019-10-09 13:21:25 +0800

    添加file-loader 添加.babelrc配置 修改webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,7 @@
-  const path = require('path')
-  const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-  const HtmlWebpackPlugin = require('html-webpack-plugin')
-  const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
 	// 入口js文件的位置
@@ -12,41 +12,61 @@ module.exports = {
 	},
 	module: {
 		rules: [
-		{
-			test: /\.js$/,
-			exclude: /(node_modules)/,
-			use: {
-				loader: "babel-loader",
-				options: {
-					presets: ["babel-preset-env"]
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
 				}
-			}
-        }
-       ]
-   },
-   plugins: [
-     new CleanWebpackPlugin({
-		 cleanStaleWebpackAssets: true
-	 }),
-	 // HTML模板的相关设置
-     new HtmlWebpackPlugin({
-        title: 'Animate',
- 		template: './src/index.html',
-		filename: 'index.html'
-     })
-   ],
-   optimization: {
-	// 压缩js文件的相关设置
-    minimizer: [new UglifyJsPlugin({
-		uglifyOptions: {
-			ie8: false,
-			output:{
-				comments: false,
-				beautify: false
 			},
-			compress: true,
-			warning: false
-		}
-	})]
-  },
+			{
+				test: /\.scss$|.css$/,
+				use: [
+					{
+						loader: 'style-loader'
+					}, {
+						loader: 'css-loader'
+					}, {
+						loader: 'sass-loader'
+					}
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader?name=./img/[name].[ext]'
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin({
+			cleanStaleWebpackAssets: true
+		}),
+		// HTML模板的相关设置
+		new HtmlWebpackPlugin({
+			title: 'Animate',
+			template: './src/index.html',
+			filename: 'index.html'
+		})
+	],
+	optimization: {
+		// 压缩js文件的相关设置
+		minimizer: [new UglifyJsPlugin({
+			uglifyOptions: {
+				ie8: false,
+				output:{
+					comments: false,
+					beautify: false
+				},
+				compress: true,
+				warning: false
+			}
+		})]
+	},
 }
